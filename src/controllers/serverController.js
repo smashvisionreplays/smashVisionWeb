f4bd09b240d62e4b35c18c351228529f2cc2e0e5
Tomas Ossa Cardenas <tomasossaefcsl@gmail.com>
2025-08-26 20:45:39 -0400

    Now gets the api url from .env

--- src/controllers/serverController.js
+++ src/controllers/serverController.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const API_BASE_URL = "http://localhost:5000/api"; // Replace with your server URL
-const API_BASE_URL = "https://api.smashvisionreplays.website/api";
+const API_BASE_URL = `${import.meta.env.VITE_API_URL}/api`;
 
 export const fetchClubs = async () => {
   try {
